85d131415641ec713eb858e9aa621dea02325f63
Ricardo Casallas <ricardo.casallas@silabs.com>
2023-08-11 10:39:41 -0400

    DEBUG IN PROGRESS: Hex debug added.

--- src/lib/support/logging/Progress.cpp
+++ src/lib/support/logging/Progress.cpp
@@ -16,3 +16,58 @@ void Progress::Debug(const char *format, ...)
 }
 
 } // namespace chip
+
+
+size_t si_format_hex(const uint8_t *input, size_t in_size, char *output, size_t out_size)
+{
+    size_t in_offset = 0;
+    size_t out_left = 0;
+    size_t out_off = 0;
+    int n;
+
+    while(out_off < out_size && in_offset < in_size)
+    {
+        out_left = out_size - out_off;
+        //printf("\nLEFT! %zu\n", out_left);
+        if(in_offset % 16 == 0)
+        {
+            if(in_offset == 0)
+            {
+                n = snprintf(&output[out_off], out_left, "%02x", (unsigned)input[in_offset]);
+            }
+            else
+            {
+                n = snprintf(&output[out_off], out_left, "  %02x", (unsigned)input[in_offset]);
+            }
+        }
+        else if(in_offset % 4 == 0)
+        {
+            n = snprintf(&output[out_off], out_left, "  %02x", (unsigned)input[in_offset]);
+        }
+        else
+        {
+            n = snprintf(&output[out_off], out_left, " %02x", (unsigned)input[in_offset]);
+        }
+        if(out_off + (size_t)n >= out_size)
+        {
+            // printf("(%zu: %zu, %zu, %d)\n", in_offset, out_size, out_off, n);
+            break;
+        }
+
+        // printf("[%zu: %zu, %zu, %d]\n", in_offset, out_size, out_off, n);
+        out_off += (size_t) n;
+        in_offset++;
+    }
+
+    // printf("<%zu: %zu, %zu, %d>\n", in_offset, out_size, out_off, n);
+    output[out_off] = 0;
+    return in_offset;
+}
+
+
+void si_debug_hex(const char *tag, const uint8_t *data, size_t data_size)
+{
+    char buffer[1024];
+    si_format_hex(data, data_size, buffer, sizeof(buffer));
+    printf("%s(%u): %s\n", tag, (unsigned) data_size, buffer);
+}
--- src/lib/support/logging/Progress.h
+++ src/lib/support/logging/Progress.h
@@ -12,3 +12,8 @@ public:
 };
 
 } // namespace chip
+
+#include <stdint.h>
+#include <stddef.h>
+size_t si_format_hex(const uint8_t *input, size_t in_size, char *output, size_t out_size);
+void si_debug_hex(const char *tag, const uint8_t *data, size_t data_size);
